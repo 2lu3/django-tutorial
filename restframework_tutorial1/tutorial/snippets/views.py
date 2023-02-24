82d98ad3b40105474b58f8fd16387fbbac961dac
2lu3 <hi2lu3@gmail.com>
2023-02-24 19:26:14 +0900

    4まで

--- restframework_tutorial1/tutorial/snippets/views.py
+++ restframework_tutorial1/tutorial/snippets/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
@@ -97,11 +97,26 @@ from rest_framework import status
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
 
+from tutorial.snippets.permissions import IsOwnerOrReadOnly
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner = self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
