fce71eb1faf0b43d47ac410f3481f22bee53b7a5
2lu3 <hi2lu3@gmail.com>
2023-03-08 18:53:45 +0900

    5まで

--- restframework_tutorial1/tutorial/snippets/views.py
+++ restframework_tutorial1/tutorial/snippets/views.py
@@ -164,3 +164,25 @@ def snippet_detail(request, pk, format=None):
     elif request.method == 'DELETE':
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+from rest_framework import renderers
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
--- restframework_tutorial1/tutorial/tutorial/settings.py
+++ restframework_tutorial1/tutorial/tutorial/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'snippets'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
